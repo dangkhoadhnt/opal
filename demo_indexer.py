82571bf594c3b1edf8026d54989534f2a589d6c2
mwitsch@gmail.com <mwitsch@gmail.com@871e0141-4e5c-27b4-8253-de9cc4e1f2df>
2012-01-19 19:22:25 +0000

    Demo indexer code enhanced: - returns results as dictionary of lists - now a count of words to take into results can be specified - architecture changed, includes dict of functions for parser function to use - indexer now takes list of lines as text input, parser subfunctions now need   to prepare data for their needs. This is suboptimal until prepared text is   cached in object for later use... - stub added to parse by lines, this is my next todo - added some documentation, well i know it needs more... - best off all: its working pretty good :D

--- demo_indexer.py
+++ demo_indexer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ### import modules
 import subprocess
 import sys
@@ -15,17 +16,37 @@ import string
 # to be searched must be cleaned from any non-printable characters a.s.o (for excample carriage return)
 # by your main programm. You also need to strip out any surplus blanks.
 class Indexer:
+    # constants
+    WORDS = 1
+    LINES = 2
+
     # initialize pattern list
     __patterns = []
+    __results = {}
+    __text = []
+    __active_pattern = {}
 
-    # add a pattern to the list of patterns
-    def addPattern(self,pattern=""):
-        self.__patterns.append(pattern)
+     # add a pattern to the list of patterns
+    def addPattern(self, pattern, mode=WORDS, count=1):
+        self.__patterns.append({'pattern': pattern, 'mode': mode, 'count': count})
 
-    # actually do the searching
-    def run(self,text=""):
+    def __run_lines(self):
+        pass
+
+    def __run_words(self):
+        # put lines together to a single string
+        text = ""
+        for line in self.__text:
+            text = text + line
+
+        # replace carriage returns with blanks to make string splittable by blanks
+        # and keep it reliably searchable
+        text = string.replace(text, "\n"," ")
+
         # split text string into its words
         wordlist = text.split(" ")
+
+        pattern = self.__active_pattern['pattern']
 
         # look for each pattern in the wordlist and return
         # the pattern and its following word in the list
@@ -33,18 +54,45 @@ class Indexer:
         # - implement a range of following words to return
         # - write findings in a list of tuples and return this list
         #   instead of printing to stdout
-        for pattern in self.__patterns:
-            print "found", wordlist.count(pattern), "appearances of", pattern
-            count = 0
-            start = 0
-            while count < wordlist.count(pattern):
-                found = wordlist.index(pattern,start)
-                start = found + 1
-                count = count + 1
+
+        # buffer to hold results for this pattern in a list
+        resultbuffer = []
+        # iteration counter
+        count = 0
+        # start index set to 0 to start at beginning of wordlist
+        start = 0
+
+        # search in wordlist for pattern until all where found, no point in searching to the end when all found
+        while count < wordlist.count(pattern):
+            #
+            found = wordlist.index(pattern,start)
+            start = found + 1
+            count = count + 1
+            resstring = ""
+            for buff in wordlist[start:start + self.__active_pattern['count']]:
+                resstring = resstring + " " + buff
+            resstring = resstring.strip()
+            if not resstring in resultbuffer:
                 try:
-                    print wordlist[found],wordlist[start]
+                    resultbuffer.append(resstring)
                 except:
-                    pass
+                    print "error in resultbuffer"
+        self.__results[pattern] = resultbuffer
+
+
+    # actually do the searching
+
+    def run(self,text=""):
+        self.__text = text
+        for pat in self.__patterns:
+            self.__active_pattern = pat
+            # create parser function dictionary, need to do this here, no way to use self outside of
+            # object method. and no way to use __run_* without self
+            # I even need to define this here, because python throws an exception, that there is no self.__active_pattern
+            # when this is defined outside loop
+            functions = {1: self.__run_words(), 2: self.__run_lines()}
+            functions[self.__active_pattern['mode']]
+        return self.__results
 
 
 ### main program
@@ -56,21 +104,14 @@ p = os.popen("pdftotext -raw /dev/stdin -")
 linebuff = p.readlines()
 p.close()
 
-# put lines together to a single string
-text = ""
-for line in linebuff:
-    text = text + line
-
-# replace carriage returns with blanks to make string splittable by blanks
-# and keep it reliably searchable
-text = string.replace(text, "\n"," ")
-
 # instantiate indexer object
 indexer = Indexer()
 # add search patterns to indexer, this is intended to work
 # with a cups test page
-indexer.addPattern("CUPS")
-indexer.addPattern("Page")
+indexer.addPattern("Patient:")
+indexer.addPattern("Entnahmedatum:", count=2)
+indexer.addPattern("folgt")
 # now run the indexer and see the results :)
-indexer.run(text)
+results = indexer.run(linebuff)
+print results
 
